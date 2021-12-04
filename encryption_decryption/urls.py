58d1d67012435bb438d1e474b91489a33e082edf
Bibekananda Bhuyan <bibekispythondeveloper@gmail.com>
2021-12-04 07:37:21 +0000

    sshgenerator

--- encryption_decryption/urls.py
+++ encryption_decryption/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("md5-generator",views.md5_generator,name="MD5 Generator"),
+    path("base64-encode",views.base64_encode_decode,name="Base 64 Encode Encoded"),
+    path("base64-decoded",views.base64_encode_decode,name="Base 64 Encode Decode"),
+    path("sha512-hash-generator",views.ssh_generators,name="SHA-512 Hash Generator"),
+    path("sha256-hash-generator",views.ssh_generators,name="SHA-256 Hash Generator"),
+    path("sha384-hash-generator",views.ssh_generators,name="SHA-384 Hash Generator"),
+    path("sha224-hash-generator",views.ssh_generators,name="SHA-224 Hash Generator"),
+    path("sha1-hash-generator",views.ssh_generators,name="SHA-1 Hash Generator"),
     
 ]
--- encryption_decryption/views.py
+++ encryption_decryption/views.py
@@ -5,6 +5,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 import requests,json
 import hashlib
+import base64
 
 
 def md5_generator(request):
@@ -12,7 +13,53 @@ def md5_generator(request):
         text=request.POST["text"]
         result = hashlib.md5(text.encode("utf-8")).hexdigest()
         return render(request, 'encryption_decryption/md5-generator.html', {
-                'output': result
+                'output': result,'input_text': text
             })
-
     return render(request,"encryption_decryption/md5-generator.html")
+
+def base64_encode_decode(request):
+    if request.method == 'POST':
+        text=request.POST["text"]
+        optype=request.POST["type"]
+        if optype=="encode":
+            optype="Encoded"
+            sample_string_bytes = text.encode("ascii")
+            base64_bytes = base64.b64encode(sample_string_bytes)
+            result = base64_bytes.decode("ascii")
+
+        else:
+            optype="Decoded"
+            base64_bytes = text.encode("ascii")
+            sample_string_bytes = base64.b64decode(base64_bytes)
+            result = sample_string_bytes.decode("ascii")
+
+        return render(request, 'encryption_decryption/base64-encode-decode.html', {
+                'output': result,'input_text': text,'op_type':optype
+            })
+    return render(request,"encryption_decryption/base64-encode-decode.html")
+
+def ssh_generators(request):
+    if request.method == 'POST':
+        text=request.POST["text"]
+        optype=request.POST["type"]
+        if optype=="SHA256":
+            optype="SHA256"
+            result = hashlib.sha256(text.encode())
+        elif optype=="SHA384":
+            optype="SHA384"
+            result = hashlib.sha384(text.encode())
+        elif optype=="SHA224":
+            optype="SHA224"
+            result = hashlib.sha224(text.encode())
+        elif optype=="SHA512":
+            optype="SHA512"
+            result = hashlib.sha512(text.encode())
+        else:
+            optype="SHA1"
+            result = hashlib.sha1(text.encode())
+         
+
+        return render(request, 'encryption_decryption/sha-hash-generator.html', {
+                'output': result.hexdigest(),'input_text': text,'op_type':optype
+            })
+    return render(request,"encryption_decryption/sha-hash-generator.html")
