42f1f7c54469128e11952e5e8d29a8bf3e69ee30
Bibekananda Bhuyan <bibekispythondeveloper@gmail.com>
2021-12-19 06:20:28 +0000

    updated with static files

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&(p$ghy^&4%lg!n0inrib2jk555j3)7g9iemm_+l=th4cb!xyv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
@@ -122,3 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'assets'),
+)
--- mainsite/urls.py
+++ mainsite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,7 @@ urlpatterns = [
     path('text-converter/', include('text_converter.urls')),
     
 ]
+urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
