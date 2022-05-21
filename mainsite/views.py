f23f2b4628ab5c9d030fc472aecc0b059b4fa651
Bibekananda Bhuyan <bibekanandabhuyan@Bibekanandas-MacBook-Pro.local>
2022-05-21 23:43:53 +0530

    updated home page

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+
+from blog.models import Blogs
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Create your views here.
+
+def index(request):
+    page_details=Blogs.objects.all()[:50]
+    page_details={"page_details":page_details}
+    return render(request, 'blog/index.html', page_details)
+
 def page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
 
-def index_home(request):
-    return render(request,"home.html")
+# def index_home(request):
+#     return render(request,"home.html")
     
 def terms_and_condition(request):
     return render(request,"terms-and-condition.html")
