d53c70cbc4e5fea55c8b2243f6661ee927673035
Bibekananda Bhuyan <bibekanandabhuyan@Bibekanandas-MacBook-Pro.local>
2022-05-22 00:05:54 +0530

    updated home page

--- mainsite/views.py
+++ mainsite/views.py
@@ -8,6 +8,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 def index_home(request):
     page_details=Blogs.objects.all()[:60]
+    page_details[0].page_titel="World News Headlines, Latest International News, World Breaking News - ToolsBand"
+    page_details[0].page_description="World News: ToolsBand news brings the latest world news headlines, Current International breaking news world wide. In depth analysis and top news headlines world wide."
+    page_details[0].keyword="news, latest news, today news, breaking news, news headlines, bollywood news, India news, top news, political news, business news, technology news, sports news"
+    page_details[0].author="Bibekananda Bhuyan"
     page_details={"page_details":page_details}
     return render(request, 'blog/index_sample.html', page_details)
 
