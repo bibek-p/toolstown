3da5a0487be9ed2cbc84df6a1e1b97f82ead869b
Bibekananda Bhuyan <bibekanandabhuyan@Bibekanandas-MacBook-Pro.local>
2022-06-03 08:16:02 +0530

    Added New Blog In Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blogs
+from django_summernote.admin import SummernoteModelAdmin
+
+
+class BlogsAdmin(SummernoteModelAdmin):
+    list_display = ['blog_heading']
+
+admin.site.register(Blogs,BlogsAdmin)
